53d41a4413432d505ea6ebb24a29789c3e2ffb81
AlanovAibek <alanov.aibek@gmail.com>
2018-05-25 04:47:18 +0300

    hotfix

--- psgan.py
+++ psgan.py
@@ -447,7 +447,8 @@ class InversePSGAN(PSGAN):
             inv_stds_z += [layers[-1].input_layer.inv_std]
         output = conv(layers[-1], self.gen_z_fn[-1], self.gen_z_ks[-1],
                       self.gen_z_W[-1], None, nonlinearity=tanh, stride=1)
-        output = lasagne.layers.Pool2DLayer(output, self.config.zx, mode='mean')
+        output = lasagne.layers.Pool2DLayer(output, self.config.zx,
+                                            mode='average_inc_pad')
         if not hasattr(self, 'means_z'):
             self.means_z = means_z
             self.inv_stds_z = inv_stds_z

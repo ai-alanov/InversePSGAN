68b6fa61181c6e955cedb23f3df25a5e5e0ebcf7
AlanovAibek <alanov.aibek@gmail.com>
2018-06-04 11:47:32 +0300

    change sample function

--- train_and_sample.py
+++ train_and_sample.py
@@ -91,5 +91,5 @@ def sample(model, config, samples_dir, texture_path,
             gen_samples = np.concatenate([imgs[i], gen_samples], axis=0)
         gen_samples = np.concatenate(gen_samples, axis=2)
         all_samples.append(gen_samples)
-    utils.save_samples(samples_dir, all_samples,
-                       [str(i+1) for i in range(n_samples)])
+    all_samples = [np.concatenate(all_samples, axis=3)]
+    utils.save_samples(samples_dir, all_samples, ['gens'])
